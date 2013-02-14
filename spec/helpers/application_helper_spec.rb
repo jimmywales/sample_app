41dfdd78663dd52d9135d2dbc552835d0eb2cc0d
jimmywales <jimmywales@ymail.com>
2013-02-14 14:41:49 -0500

    Finish layout and routes

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+describe "full_title" do
+	it "should include the page name" do
+		
+	end
+	it "should include the base name" do
+
+	end
+end
+
+end
