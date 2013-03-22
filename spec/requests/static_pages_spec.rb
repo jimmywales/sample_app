521d4597209c710e75f769774778b3e3bc2db05b
jimmywales <jimmywales@ymail.com>
2013-03-22 13:37:03 +0000

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,6 +23,18 @@ describe "Static Pages" do
 					page.should have_selector("li##{item.id}", text: item.content)
 				end
 			end
+
+			describe "follower/following counts" do
+				let(:other_user) {FactoryGirl.create(:user)}
+				before do
+					other_user.follow!(user)
+					visit root_path
+					it {should have_link("0 following", href: following_user_path(user)) }
+					it {should have_link("1 followers", href:followers_user_path(user)) }
+					it {should have_link("adfasdfa", href: 'http://www.google.com/')}
+
+				end
+			end
 		end
 
 
